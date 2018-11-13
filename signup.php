5407bccc8b4f8ad3ebb87c130271df45c234afd5
Shoma M <44600386+Shelning@users.noreply.github.com>
2018-11-13 23:59:53 +0900

    Update signup.php

--- signup.php
+++ signup.php
@@ -26,22 +26,22 @@ if (!empty($_POST["signup"])) { // 登録ボタンが押された場合
     	try {
         	 $pdo = new PDO($dsn, $db[user], $db[pass]);
 
-	   		 //ユーザーネームの重複を確認
-	                 //$stmt に同じユーザー名をもつレコードの件数を取得
-	   		 $stmt = "SELECT count(*) FROM テーブル WHERE name = '$username'";
+	         //ユーザーネームの重複を確認
+	         //$stmt に同じユーザー名をもつレコードの件数を取得
+	         $stmt = "SELECT count(*) FROM テーブル WHERE name = '$username'";
 
-	                 //fetchColum() で件数を取り出している
-	   		 $count = (int)$pdo->query($stmt)->fetchColumn();
+	         //fetchColum() で件数を取り出している
+	   	 $count = (int)$pdo->query($stmt)->fetchColumn();
 
-	   		 if ($count > 0) {
-			 	$errorMessage = 'そのユーザー名は既に使用されています';
-	   		 } else {
-	       	         	$sql = $pdo->prepare("INSERT INTO テーブル(name, password) VALUES (:name, :password)");
-	   			$sql -> bindValue(':name', $username, PDO::PARAM_STR);
-	   			$sql -> bindValue(':password', $password, PDO::PARAM_STR);
-	       		        $sql->execute();
-	       		        $signupMessage = '登録が完了しました！。';
-	   		 }
+	   	 if ($count > 0) {
+			 $errorMessage = 'そのユーザー名は既に使用されています';
+	   	 } else {
+	       	         $sql = $pdo->prepare("INSERT INTO テーブル(name, password) VALUES (:name, :password)");
+	   		 $sql -> bindValue(':name', $username, PDO::PARAM_STR);
+	   		 $sql -> bindValue(':password', $password, PDO::PARAM_STR);
+	       		 $sql->execute();
+	       		 $signupMessage = '登録が完了しました！。';
+	         }
     	 } catch (PDOException $e) { //データベースに接続できなかったとき
         	$errorMessage = 'データベースエラー';
         	// $e->getMessage() でエラー内容を参照可能（デバッグ時のみ表示）
